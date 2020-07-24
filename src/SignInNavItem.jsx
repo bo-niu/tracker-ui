84e086667cfa6b014228adde89bc1a4b290d683b
bo-niu <niubo0402@hotmail.com>
2020-07-24 01:45:20 -0700

    fixed signin bug

--- src/SignInNavItem.jsx
+++ src/SignInNavItem.jsx
@@ -17,7 +17,7 @@ class SigninNavItem extends React.Component {
     this.signIn = this.signIn.bind(this);
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     const clientId = window.ENV.GOOGLE_CLIENT_ID;
     if (!clientId) return;
     window.gapi.load('auth2', () => {
